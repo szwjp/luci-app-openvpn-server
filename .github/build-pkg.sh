7774b4ea19e48f7d039c65bac75baee140304357
szwjp <szwjp@users.noreply.github.com>
2026-08-05 20:56:42 +0800

    ci: 包文件名下划线改横线(name-version-arch 格式)

--- .github/build-pkg.sh
+++ .github/build-pkg.sh
@@ -145,9 +145,9 @@ default_prerm
 		--script "pre-deinstall:$TEMP_DIR/pre-deinstall" \
 		--info "depends:$APK_DEPENDS" \
 		--files "$TEMP_PKG_DIR" \
-		--output "$TEMP_DIR/${PKG_NAME}_${PKG_VERSION}.apk"
+		--output "$TEMP_DIR/${PKG_NAME}-${PKG_VERSION}.apk"
 
-	mv "$TEMP_DIR/${PKG_NAME}_${PKG_VERSION}.apk" "$BASE_DIR/${PKG_NAME}_${PKG_VERSION}_all.apk"
+	mv "$TEMP_DIR/${PKG_NAME}-${PKG_VERSION}.apk" "$BASE_DIR/${PKG_NAME}-${PKG_VERSION}-all.apk"
 else
 	cat > "$TEMP_PKG_DIR/CONTROL/control" <<-EOF
 		Package: $PKG_NAME
@@ -195,7 +195,7 @@ default_prerm $0 $@
 
 	ipkg-build -m "" "$TEMP_PKG_DIR" "$TEMP_DIR"
 
-	mv "$TEMP_DIR/${PKG_NAME}_${PKG_VERSION}_all.ipk" "$BASE_DIR/${PKG_NAME}_${PKG_VERSION}_all.ipk"
+	mv "$TEMP_DIR/${PKG_NAME}_${PKG_VERSION}_all.ipk" "$BASE_DIR/${PKG_NAME}-${PKG_VERSION}-all.ipk"
 fi
 
 # 中文翻译子包
@@ -212,8 +212,8 @@ if [ "$PKG_MGR" == "apk" ]; then
 		--info "arch:noarch" \
 		--info "depends:$PKG_NAME" \
 		--files "$I18N_DIR" \
-		--output "$TEMP_DIR/${I18N_NAME}_${PKG_VERSION}.apk"
-	mv "$TEMP_DIR/${I18N_NAME}_${PKG_VERSION}.apk" "$BASE_DIR/${I18N_NAME}_${PKG_VERSION}_all.apk"
+		--output "$TEMP_DIR/${I18N_NAME}-${PKG_VERSION}.apk"
+	mv "$TEMP_DIR/${I18N_NAME}-${PKG_VERSION}.apk" "$BASE_DIR/${I18N_NAME}-${PKG_VERSION}-all.apk"
 else
 	mkdir -p "$I18N_DIR/CONTROL/"
 	cat > "$I18N_DIR/CONTROL/control" <<-EOFCTRL
@@ -224,7 +224,7 @@ else
 		Description: OpenVPN Server Chinese translation
 	EOFCTRL
 	ipkg-build -m "" "$I18N_DIR" "$TEMP_DIR"
-	mv "$TEMP_DIR/${I18N_NAME}_${PKG_VERSION}_all.ipk" "$BASE_DIR/${I18N_NAME}_${PKG_VERSION}_all.ipk"
+	mv "$TEMP_DIR/${I18N_NAME}_${PKG_VERSION}_all.ipk" "$BASE_DIR/${I18N_NAME}-${PKG_VERSION}-all.ipk"
 fi
 
 rm -rf "$TEMP_DIR"
